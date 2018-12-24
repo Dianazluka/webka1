90c0371f152703be11eac46e0d0f069ce7957d7b
Dianazluka <deedee123@mail.ru>
2018-12-25 02:59:31 +0600

    Update web_db_12_12_18.sql

--- db/web_db_12_12_18.sql
+++ db/web_db_12_12_18.sql
@@ -4,7 +4,96 @@
 -- ------------------------------------------------------
 -- Server version	5.1.73
 
+--
+-- Table structure for table `notes`
+--
 
+DROP TABLE IF EXISTS `notes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `description` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
+  `date_target` datetime NOT NULL,
+  `date_create` datetime NOT NULL,
+  `complete` int(1) NOT NULL DEFAULT '0',
+  `date_complete` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notes`
+--
+
+LOCK TABLES `notes` WRITE;
+/*!40000 ALTER TABLE `notes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `notes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sessions`
+--
+
+DROP TABLE IF EXISTS `sessions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(50) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `token` (`token`)
+) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sessions`
+--
+
+LOCK TABLES `sessions` WRITE;
+/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
+INSERT INTO `sessions` VALUES (27,'2e6b7ef39d3e4f2e5332e5290ff8224b',35,'2018-10-27 16:48:57'),(28,'018bfdfaacef5ba546875ac0b5299c83',35,'2018-10-27 16:49:44'),(30,'45feebe72ceb361bd026f58ebbfa0e14',35,'2018-11-26 14:35:14');
+/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) DEFAULT '',
+  `lastname` varchar(50) DEFAULT '',
+  `birthdate` datetime DEFAULT NULL,
+  `login` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  `active` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (35,'','',NULL,'petr','petr@mail.ru','e9bc0e13a8a16cbb07b175d92a113126','2018-10-24 16:42:45','2018-10-24 16:42:45',1),(36,'','',NULL,'chelovek','chelovek@mail.ru','eec1ebc4246f04c3161ccc36fc6ab606','2018-10-24 16:47:25','2018-10-24 16:47:25',1),(37,'','',NULL,'zimzum','zimzum@mail.ru','e9bc0e13a8a16cbb07b175d92a113126','2018-11-27 08:36:50','2018-11-27 08:36:50',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `section`
