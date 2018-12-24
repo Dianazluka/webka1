26367d0c28b110cbdc4dae2d4c61b4361fdee0be
Dianazluka <deedee123@mail.ru>
2018-12-25 02:18:34 +0600

    Update web_db.sql

--- db/web_db.sql
+++ db/web_db.sql
@@ -4,29 +4,50 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `web_db` DEFAULT CHARACTER SET utf8 COLLATE 'utf8_general_ci';
 USE `web_db`;
 
-DROP TABLE IF EXISTS `sessions`;
-CREATE TABLE IF NOT EXISTS `sessions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `token` varchar(50) NOT NULL,
-  `user_id` int(10) unsigned NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `token` (`token`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `section`;
+CREATE TABLE IF NOT EXISTS `section` (
+  `section_id` int(11) NOT NULL AUTO_INCREMENT,
+  `section_name` varchar(100) NOT NULL,
+   `membership` varchar(20) NOT NULL,
+  `place_employment` varchar(50) NOT NULL,
+  `reception_features` varchar(50) NOT NULL,
+  `trainer` varchar(50) NOT NULL,
+  `payment` varchar(20) NOT NULL,
+    PRIMARY KEY (`id`),
+  ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `firstname` varchar(50) DEFAULT '',
-  `lastname` varchar(50) DEFAULT '',
-  `birthdate` datetime DEFAULT NULL,
-  `login` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `create_date` datetime NOT NULL,
-  `update_date` datetime NOT NULL,
-  `active` int(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `login` (`login`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+  `sportsman_id` int(11) NOT NULL AUTO_INCREMENT,
+  `s_section_id` int(10) unsigned NOT NULL,
+  `surname` varchar(100) DEFAULT '',
+  `age` int(11) DEFAULT '',
+  `address` varchar(20) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `growth` varchar(20) NOT NULL,
+  `weight` varchar(20) NOT NULL,
+  `achievement` varchar(20) NOT NULL,
+    PRIMARY KEY (`sportsman_id`),
+  UNIQUE KEY `s_section_id` (`s_section_id`),
+ ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `competition`;
+CREATE TABLE IF NOT EXISTS `competition` (
+  `competition_id` int(11) NOT NULL AUTO_INCREMENT,
+  `date_competition` datetime NOT NULL,
+   `number_participants` varchar(20) DEFAULT '',
+  `rank` varchar(20) DEFAULT '',
+      PRIMARY KEY (`competition_id`),
+   ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+   
+   DROP TABLE IF EXISTS `participation`;
+CREATE TABLE IF NOT EXISTS `participation` (
+  `participation_id` int(11) NOT NULL AUTO_INCREMENT,
+  `p_sportsman_id` int(10) unsigned NOT NULL,
+   `p_сompetition_id` int(10) unsigned NOT NULL,
+  `result` varchar(20) DEFAULT '',
+  `place` varchar(20) NOT NULL,
+     PRIMARY KEY (`participation_id`),
+  UNIQUE KEY `p_sportsman_id` (`p_sportsman_id`),
+  UNIQUE KEY `p_сompetition_id` (`p_сompetition_id`),
+ ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
