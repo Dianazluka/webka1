c56de0e3a51b3e030943ac5eb15547b868bfdef1
Dianazluka <deedee123@mail.ru>
2018-12-25 03:01:01 +0600

    web_bd_hm.sql

--- db/web_db.sql
+++ db/web_db.sql
@@ -4,6 +4,47 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `web_db` DEFAULT CHARACTER SET utf8 COLLATE 'utf8_general_ci';
 USE `web_db`;
 
+DROP TABLE IF EXISTS `notes`;
+CREATE TABLE `notes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `description` varchar(250) CHARACTER SET utf8 DEFAULT NULL,
+  `date_target` datetime NOT NULL,
+  `date_create` datetime NOT NULL,
+  `complete` int(1) NOT NULL DEFAULT '0',
+  `date_complete` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `token` varchar(50) NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `token` (`token`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(50) DEFAULT '',
+  `lastname` varchar(50) DEFAULT '',
+  `birthdate` datetime DEFAULT NULL,
+  `login` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  `active` int(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `section`;
 CREATE TABLE IF NOT EXISTS `section` (
   `section_id` int(11) NOT NULL AUTO_INCREMENT,
